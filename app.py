bf59a8d40237522c6659f0848075be2e7682b0c1
Subarna Bajracharya <subarna.bajracharya@patancollege.edu.np>
2024-05-04 12:36:14 +0545

    made some minor changes

--- app.py
+++ app.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/aboutus/')
+def aboutus():
+    return render_template('aboutus.html')
+
+@app.route('/contactus/')
+def contactus():
+    return render_template('contactus.html')
+
+@app.route('/social/')
+def socials():
+    return render_template('socials.html')
+
 if __name__ == '__main__':
     app.run(debug = True)
